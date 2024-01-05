27b3eecc50361be4a27f01a2b40af295a9a65fc9
100ke <bhy9803@gmail.com>
2024-01-05 18:03:41 +0900

    240105 login

--- src/api/MemberApi.js
+++ src/api/MemberApi.js
@@ -1,12 +1,29 @@
 import axios from "axios";
 
-const baseURL = 'http://localhost:8081'
+const baseURL = process.env.REACT_APP_BASE_URL;
+const ACCESS_TOKEN = localStorage.getItem('ACCESS_TOKEN');
+
 const axiosInstance = axios.create({
     baseURL,
     headers:{
         'Content-Type':'application/json',
+        'Authorization' : ACCESS_TOKEN ? `Bearer ${ACCESS_TOKEN}` : '',
     }
 })
+
+// login
+export const postLogin = async (data)=> {
+    try {
+        const response = await axiosInstance.post("/login", data)
+        console.log(response.data.token);
+        if(response.data.token) {
+            localStorage.setItem("ACCESS_TOKEN", response.data.token)
+            window.location.href = "/"
+        }
+    } catch (error) {
+        console.error("로그인 에러 : ", error);
+    }
+}
 
 export const getMemberList = async ()=> {
     try {
@@ -16,3 +33,4 @@ export const getMemberList = async ()=> {
         throw error
     }
 }
+
