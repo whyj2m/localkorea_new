2126d5a4aec1bd41b48389b2e3778066603deb06
yoon <whyjmlearn@gmail.com>
2024-01-30 21:00:03 +0900

    브런치 오류? 테스트

--- src/api/LocalPlaceApi.js
+++ src/api/LocalPlaceApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:8081";
+const baseURL = process.env.REACT_APP_BASE_URL;
 const axiosInstance = axios.create({
   baseURL,
   headers: {
